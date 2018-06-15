9384e44d1b17827e8f7a6329bf9fa5cb99f0e0a3
Nate Flood <nate@famconnect.net>
2018-06-15 17:02:24 -0400

    Deletes Messages

--- lambda.js
+++ lambda.js
@@ -1,3 +1,30 @@
-exports.handler = function (event, context) {
-	context.succeed('hello world');
+var AWS = require("aws-sdk");
+AWS.config.update({region: 'us-east-1'});
+
+var TASK_QUEUE_URL = process.env.TASK_QUEUE_URL;
+
+var sqs = new AWS.SQS();
+var s3 = new AWS.S3();
+
+function deleteMessage(receiptHandle, callback) {
+  sqs.deleteMessage({
+    ReceiptHandle: receiptHandle,
+    QueueUrl: TASK_QUEUE_URL
+  }, callback);
+}
+
+function work(task, callback) {
+  console.log(task);
+  // TODO implement
+  callback();
+}
+
+exports.handler = function(event, context, callback) {
+  work(event.Body, function(err) {
+    if (err) {
+      callback(err);
+    } else {
+      deleteMessage(event.ReceiptHandle, callback);
+    }
+  });
 };
